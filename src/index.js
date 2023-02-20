6dbf8530bc1cdb15297decaf3662779860576653
LiliaSlabka <liaslabka@gmail.com>
2023-02-20 15:02:23 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -22,8 +22,7 @@ function handleInput(value) {
     fetchCountries(value)
         .then(countries => {
             if (countries.length > 10) {
-                Notiflix.Notify.failure(
-                    'Too many matches found. Please enter a more specific name.')
+                Notiflix.Notify.failure('Too many matches found. Please enter a more specific name.')
             }
             else if (countries <= 10 && countries.length >= 2) {
                 displayCountriesInfo(countries)
@@ -41,15 +40,24 @@ function displayCountryInfo(countries) {
     const markup = countries
         .map(country => {
             return `<li>
-      <img src="${country.flags.svg}" alt="Flag of ${country.name.official
-                }" width="30" hight="20">
+      <img src="${country.flags.svg}" alt="Flag of${country.name.official}" width="30" hight="20">
             <b>${country.name.official}</b>
             <p><b>Capital</b>: ${country.capital}</p>
             <p><b>Population</b>: ${country.population}</p>
-            <p><b>Languages</b>: ${Object.values(country.languages)} </p>
-                </li>`;
+            <p><b>Languages</b>: ${Object.values(country.languages)} </p></li>`;
         })
         .join('');
     countryList.innerHTML = markup;
 }
 
+function displayCountriesInfo(countries) {
+    const markup = countries
+        .map(country => {
+            return `<li>
+      <img src="${country.flags.svg}" alt="Flag of ${country.name.official}" width="30" height="20">
+        <b>${country.name.official}</b>
+                </li>`
+        })
+        .join('');
+    countryList.innerHTML = markup;
+}
